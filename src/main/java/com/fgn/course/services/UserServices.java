51823ce557445933620479a556156b8b0c1216a5
mephistosz <felipe.fg1@outlook.com>
2022-04-18 18:55:05 -0300

    Service layer, component registration

---
+++ src/main/java/com/fgn/course/services/UserServices.java
@@ -0,0 +1,27 @@
+package com.fgn.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fgn.course.entities.User;
+import com.fgn.course.repositories.UserRepository;
+
+@Service
+public class UserServices {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
