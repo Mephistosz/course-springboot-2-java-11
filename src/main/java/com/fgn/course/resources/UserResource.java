65f3c655872c03d14740ca00618131eface570bf
mephistosz <felipe.fg1@outlook.com>
2022-04-23 18:02:12 -0300

    User insert

--- src/main/java/com/fgn/course/resources/UserResource.java
+++ src/main/java/com/fgn/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.fgn.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fgn.course.entities.User;
 import com.fgn.course.services.UserServices;
@@ -29,6 +33,15 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		// deixar no response header qual o caminho da requisição complçeta e colocar o status code como 201 created
+		// o path fala que ele vai ter depois do users um id no qual ele pega no outro metodo a seguir
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
